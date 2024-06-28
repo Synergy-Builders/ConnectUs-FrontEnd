80c397b9ae323a4f61dac6380289f8e06afe081b
typicalninja <65993466+typicalninja@users.noreply.github.com>
2024-06-29 00:38:34 +0530

    refactor: add react-router

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import Home from './Home.js'
-
-function App() {
-  return <Home/>
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// routes
+import Root from "./routes/root"
+
+
+// https://reactrouter.com/en/main/start/overview
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
